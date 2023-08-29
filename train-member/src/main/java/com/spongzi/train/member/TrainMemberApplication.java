0f73d79a516dab93b5d29a795d31d7cca9b4c9b2
suxu <spongziw@163.com>
2023-08-29 11:15:49 +0800

    将AOP提取到common模块，并且将pom文件中的公共部分也提取到common模块中区

--- train-member/src/main/java/com/spongzi/train/member/TrainMemberApplication.java
+++ train-member/src/main/java/com/spongzi/train/member/TrainMemberApplication.java
@@ -3,10 +3,12 @@ package com.spongzi.train.member;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 @Slf4j
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.spongzi.*"})
 public class TrainMemberApplication {
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(TrainMemberApplication.class);
