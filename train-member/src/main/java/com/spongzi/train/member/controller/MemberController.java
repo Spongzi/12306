7aceadd6fd97132d25434a67ea03227f19cf0c47
suxu <spongziw@163.com>
2023-08-29 14:59:12 +0800

    实现一个简单的注册接口

--- train-member/src/main/java/com/spongzi/train/member/controller/MemberController.java
+++ train-member/src/main/java/com/spongzi/train/member/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.spongzi.train.member.controller;
 import com.spongzi.train.member.service.MemberService;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +21,12 @@ public class MemberController {
     private MemberService memberService;
 
     @GetMapping("/count")
-    public Long hello() {
+    public Long count() {
         return memberService.count();
+    }
+
+    @PostMapping("/registry")
+    public Long registry(String mobile) {
+        return memberService.registry(mobile);
     }
 }
--- train-member/src/main/java/com/spongzi/train/member/service/MemberService.java
+++ train-member/src/main/java/com/spongzi/train/member/service/MemberService.java
@@ -1,8 +1,13 @@
 package com.spongzi.train.member.service;
 
+import cn.hutool.core.collection.CollUtil;
+import com.spongzi.train.member.domain.Member;
+import com.spongzi.train.member.domain.MemberExample;
 import com.spongzi.train.member.mapper.MemberMapper;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MemberService {
@@ -15,7 +20,29 @@ public class MemberService {
      *
      * @return int
      */
-    public long count() {
+    public Long count() {
         return memberMapper.countByExample(null);
+    }
+
+    /**
+     * 注册表
+     *
+     * @param mobile 移动
+     * @return {@link Long} 返回注册用户的id
+     */
+    public Long registry(String mobile) {
+        MemberExample memberExample = new MemberExample();
+        memberExample.createCriteria().andMobileEqualTo(mobile);
+
+        List<Member> members = memberMapper.selectByExample(memberExample);
+        if (CollUtil.isNotEmpty(members)) {
+            throw new RuntimeException("mobile is registry");
+        }
+
+        Member member = new Member();
+        member.setId(System.currentTimeMillis());
+        member.setMobile(mobile);
+        memberMapper.insert(member);
+        return member.getId();
     }
 }
