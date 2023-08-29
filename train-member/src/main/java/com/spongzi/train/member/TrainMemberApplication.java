d82e2a436a5a8eab1dc0ef5bae558b8ccb696342
suxu <spongziw@163.com>
2023-08-29 10:37:06 +0800

    增加日志信息

--- train-member/src/main/java/com/spongzi/train/member/TrainMemberApplication.java
+++ train-member/src/main/java/com/spongzi/train/member/TrainMemberApplication.java
@@ -1,11 +1,17 @@
 package com.spongzi.train.member;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
+@Slf4j
 @SpringBootApplication
 public class TrainMemberApplication {
     public static void main(String[] args) {
-        SpringApplication.run(TrainMemberApplication.class, args);
+        SpringApplication app = new SpringApplication(TrainMemberApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        log.info("启动成功!");
+        log.info("地址：\thttp://localhost:{}", env.getProperty("server.port"));
     }
 }
