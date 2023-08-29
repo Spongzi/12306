78d624b20e1a57aff98a4975fc893bc7ecb24d79
suxu <spongziw@163.com>
2023-08-29 15:40:36 +0800

    添加项目统一的异常处理

---
+++ train-common/src/main/java/com/spongzi/train/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,59 @@
+package com.spongzi.train.common.exception;
+
+import com.spongzi.train.common.resp.CommonResp;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 统一异常处理、数据预处理等
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    private static final Logger LOG = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    /**
+     * 所有异常统一处理
+     *
+     * @param e 拦截到的错误信息
+     * @return 返回抛出的异常信息
+     */
+    @ExceptionHandler(value = Exception.class)
+    @ResponseBody
+    public CommonResp<Object> exceptionHandler(Exception e) {
+        return CommonResp.builder().success(false).message("系统出现异常，请联系管理员").build();
+    }
+
+    /**
+     * 业务异常统一处理
+     *
+     * @param e 拦截到的错误信息
+     * @return 返回抛出的一信息
+     */
+    @ExceptionHandler(value = BusinessException.class)
+    @ResponseBody
+    public CommonResp<Object> exceptionHandler(BusinessException e) {
+        return CommonResp.builder().success(false).message(e.getE().getDesc()).build();
+    }
+
+    /**
+     * 校验异常统一处理
+     *
+     * @param e 拦截到的错误信息
+     * @return 返回抛出的一信息
+     */
+    @ExceptionHandler(value = BindException.class)
+    @ResponseBody
+    public CommonResp<Object> exceptionHandler(BindException e) {
+        LOG.error("校验异常：{}", e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
+        return CommonResp.builder()
+                .success(false)
+                .message(e.getBindingResult().getAllErrors().get(0).getDefaultMessage())
+                .build();
+    }
+
+}
