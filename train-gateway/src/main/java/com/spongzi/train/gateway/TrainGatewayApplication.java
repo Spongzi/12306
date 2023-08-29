cc156230755c0be73c590f011cd6498c814fddde
suxu <spongziw@163.com>
2023-08-29 11:22:42 +0800

    增加网关模块gateway

--- train-gateway/src/main/java/com/spongzi/train/gateway/TrainGatewayApplication.java
+++ train-gateway/src/main/java/com/spongzi/train/gateway/TrainGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.spongzi.train.member;
+package com.spongzi.train.gateway;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
@@ -9,11 +9,11 @@ import org.springframework.core.env.Environment;
 @Slf4j
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.spongzi.*"})
-public class TrainMemberApplication {
+public class TrainGatewayApplication {
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(TrainMemberApplication.class);
+        SpringApplication app = new SpringApplication(TrainGatewayApplication.class);
         Environment env = app.run(args).getEnvironment();
         log.info("启动成功!");
-        log.info("会员模块地址：\thttp://localhost:{}", env.getProperty("server.port"));
+        log.info("网关地址：\thttp://localhost:{}", env.getProperty("server.port"));
     }
 }
