d96254ba5eddee038e37f38e091bec2e9de8e666
suxu <spongziw@163.com>
2023-09-04 14:59:31 +0800

    禁止任务并发执行

--- train-batch/src/main/java/com/spongzi/train/batch/job/TestJob.java
+++ train-batch/src/main/java/com/spongzi/train/batch/job/TestJob.java
@@ -1,9 +1,11 @@
 package com.spongzi.train.batch.job;
 
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
+@DisallowConcurrentExecution // 禁用并发执行
 public class TestJob implements Job {
 
     @Override
