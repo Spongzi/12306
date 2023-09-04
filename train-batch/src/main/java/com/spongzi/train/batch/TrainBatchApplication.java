7b0f11fc6332f4221168cad72bd493980890129c
suxu <spongziw@163.com>
2023-09-04 16:38:37 +0800

    集成openfeign，实现为服务间调用。给每个应用增加应用名，可搭配配置中心使用

--- train-batch/src/main/java/com/spongzi/train/batch/TrainBatchApplication.java
+++ train-batch/src/main/java/com/spongzi/train/batch/TrainBatchApplication.java
@@ -2,8 +2,10 @@ package com.spongzi.train.batch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("com.spongzi.train.batch.feign")
 public class TrainBatchApplication {
     public static void main(String[] args) {
         SpringApplication.run(TrainBatchApplication.class, args);
---
+++ train-batch/src/main/java/com/spongzi/train/batch/feign/BusinessFeign.java
@@ -0,0 +1,8 @@
+package com.spongzi.train.batch.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+
+@FeignClient(name = "business", url = "http://127.0.0.1:8002/business")
+public interface BusinessFeign {
+
+}
