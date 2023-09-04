ffc2fe17471a7b238d274e35ea31c5cdf3223c7b
suxu <spongziw@163.com>
2023-09-04 14:23:34 +0800

    增加batch定时调度模块

---
+++ train-batch/src/main/java/com/spongzi/train/batch/TrainBatchApplication.java
@@ -0,0 +1,11 @@
+package com.spongzi.train.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainBatchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TrainBatchApplication.class, args);
+    }
+}
