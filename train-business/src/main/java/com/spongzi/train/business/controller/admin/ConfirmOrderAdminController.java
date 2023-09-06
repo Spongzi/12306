7c90b09c8efa3e55cac24f96c281216e19dd4a5a
Spongzi <lxlandsx@outlook.com>
2023-09-06 10:41:31 +0800

    保存确认订单，状态初始

--- train-business/src/main/java/com/spongzi/train/business/controller/admin/ConfirmOrderAdminController.java
+++ train-business/src/main/java/com/spongzi/train/business/controller/admin/ConfirmOrderAdminController.java
@@ -1,38 +1,25 @@
 package com.spongzi.train.business.controller.admin;
 
-import com.spongzi.train.common.context.LoginMemberContext;
-import com.spongzi.train.common.resp.CommonResp;
-import com.spongzi.train.common.resp.PageResp;
-import com.spongzi.train.business.req.ConfirmOrderQueryReq;
-import com.spongzi.train.business.req.ConfirmOrderSaveReq;
-import com.spongzi.train.business.resp.ConfirmOrderQueryResp;
+import com.spongzi.train.business.req.ConfirmOrderDoReq;
 import com.spongzi.train.business.service.ConfirmOrderService;
+import com.spongzi.train.common.resp.CommonResp;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/admin/confirm-order")
+@RequestMapping("/confirm-order")
 public class ConfirmOrderAdminController {
 
     @Resource
     private ConfirmOrderService confirmOrderService;
 
-    @PostMapping("/save")
-    public CommonResp<Object> save(@Valid @RequestBody ConfirmOrderSaveReq req) {
-        confirmOrderService.save(req);
-        return CommonResp.builder().build();
-    }
-
-    @GetMapping("/query-list")
-    public CommonResp<PageResp<ConfirmOrderQueryResp>> queryList(@Valid ConfirmOrderQueryReq req) {
-        PageResp<ConfirmOrderQueryResp> list = confirmOrderService.queryList(req);
-        return CommonResp.<PageResp<ConfirmOrderQueryResp>>builder().content(list).build();
-    }
-
-    @DeleteMapping("/delete/{id}")
-    public CommonResp<Object> delete(@PathVariable Long id) {
-        confirmOrderService.delete(id);
+    @PostMapping("/do")
+    public CommonResp<Object> doConfirm(@Valid @RequestBody ConfirmOrderDoReq req) {
+        confirmOrderService.doConfirm(req);
         return CommonResp.builder().build();
     }
 
--- train-business/src/main/java/com/spongzi/train/business/service/ConfirmOrderService.java
+++ train-business/src/main/java/com/spongzi/train/business/service/ConfirmOrderService.java
@@ -3,8 +3,11 @@ package com.spongzi.train.business.service;
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
 import cn.hutool.core.util.ObjectUtil;
+import com.alibaba.fastjson.JSON;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import com.spongzi.train.business.req.ConfirmOrderDoReq;
+import com.spongzi.train.common.context.LoginMemberContext;
 import com.spongzi.train.common.resp.PageResp;
 import com.spongzi.train.common.utils.SnowUtil;
 import com.spongzi.train.business.domain.ConfirmOrder;
@@ -18,7 +21,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
+
+import static com.spongzi.train.business.enums.ConfirmOrderStatusEnum.INIT;
 
 @Service
 public class ConfirmOrderService {
@@ -64,7 +70,41 @@ public class ConfirmOrderService {
         return pageResp;
     }
 
-    public void delete(Long id) {
-        confirmOrderMapper.deleteByPrimaryKey(id);
+    public void doConfirm(ConfirmOrderDoReq req) {
+        // 省略业务校验，如：车次是否存在，余票是否存在，车次是否在有效期内，tickets条数>0，同乘客是否已经买过
+
+        Date now = DateTime.now();
+        // 保存确认订单，状态初始
+        ConfirmOrder confirmOrder = new ConfirmOrder();
+        confirmOrder.setId(SnowUtil.getSnowflakeNextId());
+        confirmOrder.setMemberId(LoginMemberContext.getId());
+        confirmOrder.setDate(req.getDate());
+        confirmOrder.setTrainCode(req.getTrainCode());
+        confirmOrder.setStart(req.getStart());
+        confirmOrder.setEnd(req.getEnd());
+        confirmOrder.setDailyTrainTicketId(req.getDailyTrainTicketId());
+        confirmOrder.setStatus(INIT.getCode());
+        confirmOrder.setCreateTime(now);
+        confirmOrder.setUpdateTime(now);
+        confirmOrder.setTickets(JSON.toJSONString(req.getTickets()));
+
+        confirmOrderMapper.insert(confirmOrder);
+
+        // 查出余票记录，需要得到真实的库存
+
+        //  扣减余票数量，预减而不是真的减
+
+        // 选座
+
+            // 一个车厢一个车厢的获取
+
+            // 挑选符合条件的座位，如果这个车厢不满足，则进入下一个车厢（多个选座应该在同一个车厢内）
+
+        // 对选中的数据进行事务处理：
+
+            // 座位表修改售卖情况
+            // 余票详情表修改余票
+            // 为会员增加购票记录
+            // 更新确认订单表位成功
     }
 }
